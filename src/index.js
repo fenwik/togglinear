255da97b831bb7000a84d5d4c2b286726d3a5907
Dmitry Tagakov <dt.medicin@gmail.com>
2021-02-23 17:46:21 +0700

    Add `--help` and `--version` options.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import arg from 'arg';
 import chalk from 'chalk';
+import optionator from 'optionator';
 
+import { version } from '../package.json';
 import {
   setConfig,
   deleteConfigFile,
@@ -11,21 +12,39 @@ import getIssue from './getIssue';
 import searchIssue from './searchIssue';
 import startTimeEntry from './startTimeEntry';
 
-const seq = async({
-  resetConfig,
-  resetProjectsMapping,
-  issueId
-}) => {
-  if (resetConfig) {
-    await deleteConfigFile();
-  }
+const CLIOptions = optionator({
+  prepend: 'togglinear [taskID]',
+  options: [
+    {
+      option: 'version',
+      alias: 'v',
+      type: 'Boolean',
+      description: 'Output the version number'
+    },
+    {
+      option: 'help',
+      alias: 'h',
+      type: 'Boolean',
+      description: 'Show help'
+    },
+    {
+      option: 'reset-config',
+      alias: 'rc',
+      type: 'Boolean',
+      description: 'Delete config file with Linear API key, Toggl API token and projects mapping'
+    },
+    {
+      option: 'reset-projects-mapping',
+      alias: 'rpm',
+      type: 'Boolean',
+      description: 'Delete projects map between Linear and Toggl'
+    }
+  ]
+});
 
+const startTimeTrack = async(issueId) => {
   const config = await getConfig();
   setConfig(config);
-
-  if (resetProjectsMapping) {
-    await deleteProjectsMapping();
-  }
 
   const issue = issueId ? await getIssue(issueId) : await searchIssue();
 
@@ -41,24 +60,40 @@ const seq = async({
     + `\n${project ? chalk.hex(project.hex_color.toUpperCase()).bold(project.name) : ''}\n`);
 };
 
-const cli = (argv) => {
-  const args = arg({
-    '--reset-config': Boolean,
-    '--reset-projects-mapping': Boolean,
-    '--rc': '--reset-config',
-    '--rpm': '--reset-projects-mapping'
-  },
-  {
-    argv
-  });
+const cli = (args) => {
+  const options = CLIOptions.parseArgv(args);
 
-  const options = {
-    resetConfig: args['--reset-config'] || false,
-    resetProjectsMapping: args['--reset-projects-mapping'] || false,
-    issueId: args._[2] || null
-  };
+  if (options.version) {
+    console.log(`Togglinear CLI version: ${chalk.green(version)}`);
+    return;
+  }
 
-  seq(options).catch((error) => {
+  if (options.help) {
+    console.log(CLIOptions.generateHelp());
+    return;
+  }
+
+  if (options.resetConfig) {
+    deleteConfigFile().then(() => {
+      console.log('Config file deleted successfully');
+    }).catch((error) => {
+      console.error(error);
+    });
+    return;
+  }
+
+  if (options.resetProjectsMapping) {
+    deleteProjectsMapping().then(() => {
+      console.log('Projects mapping deleted successfully');
+    }).catch((error) => {
+      console.error(error);
+    });
+    return;
+  }
+
+  const issueId = options._[0] || null;
+
+  startTimeTrack(issueId).catch((error) => {
     console.error(error);
   });
 };
